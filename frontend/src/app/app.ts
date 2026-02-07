414dfd1bd17e495aad06fe34713b03a1a3a812ed
NicolasBaez <114032@tecnicatura.frc.utn.edu.ar>
2026-02-07 17:35:54 -0300

    Add navbar component and make App standalone

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,17 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent],
+  template: `
+    <app-navbar></app-navbar>
+    <router-outlet></router-outlet>
+  `,
+  styles: [],
 })
-export class App {
-  protected readonly title = signal('frontend');
+export class AppComponent {
+  title = 'CineBook';
 }
