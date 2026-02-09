fd1075abb543150c6fc81478a7b447394a2b2020
NicolasBaez <114032@tecnicatura.frc.utn.edu.ar>
2026-02-09 09:19:52 -0300

    Add seats and bookings methods to ApiService

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -1,17 +1,19 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Movie } from '../models/movie.model';
+import { BookingRequest, BookingResponse } from '../models/booking.model';
 import { Cinema } from '../models/cinema.model';
+import { Movie } from '../models/movie.model';
+import { Seat } from '../models/seat.model';
 import { Showtime } from '../models/showtime.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
   private apiUrl = 'http://localhost:8080/api';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   // MOVIES
   getMovies(): Observable<Movie[]> {
@@ -38,5 +40,23 @@ export class ApiService {
 
   getShowtimeById(id: number): Observable<Showtime> {
     return this.http.get<Showtime>(`${this.apiUrl}/showtimes/${id}`);
+  }
+
+  // SEATS
+  getSeatsByShowtime(showtimeId: number): Observable<Seat[]> {
+    return this.http.get<Seat[]>(`${this.apiUrl}/seats/showtime/${showtimeId}`);
+  }
+
+  getAvailableSeats(showtimeId: number): Observable<Seat[]> {
+    return this.http.get<Seat[]>(`${this.apiUrl}/seats/showtime/${showtimeId}/available`);
+  }
+
+  // BOOKINGS
+  createBooking(userName: string, booking: BookingRequest): Observable<BookingResponse> {
+    return this.http.post<BookingResponse>(`${this.apiUrl}/bookings?userName=${userName}`, booking);
+  }
+
+  getMyBookings(userName: string): Observable<BookingResponse[]> {
+    return this.http.get<BookingResponse[]>(`${this.apiUrl}/bookings/user/${userName}`);
   }
 }
