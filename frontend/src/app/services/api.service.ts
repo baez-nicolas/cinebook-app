f60b6ea9f85caaca72c3316fa891b0a4b12993bc
NicolasBaez <114032@tecnicatura.frc.utn.edu.ar>
2026-02-14 01:04:38 -0300

    Use environment API URL and allow Railway CORS

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 import { BookingRequest, BookingResponse } from '../models/booking.model';
 import { Cinema } from '../models/cinema.model';
 import { Movie } from '../models/movie.model';
@@ -11,7 +12,7 @@ import { Showtime } from '../models/showtime.model';
   providedIn: 'root',
 })
 export class ApiService {
-  private apiUrl = 'http://localhost:8080/api';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
 
